751e7ca4bec0f00f8da516cad7acd8bbc7806970
Sander <sandermoolin@gmail.com>
2014-05-27 19:49:51 -0400

    Added timer

---
+++ scripts/gameplay/RaceTimer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RaceTimer : MonoBehaviour {
+
+	Timer countdown;
+	public int visibleTimer;
+
+	private float startTime = -1;
+
+	private void Start ()
+	{
+		countdown = new Timer (1f, visibleTimer);
+	}
+
+	private string DisplayAsStopwatch (float time)
+	{
+		string result = string.Empty;
+		result = string.Format ("{0}:{1:00.000}",
+		                        Math.Floor(time / 60f).ToString(),
+		                        Math.Round (time % 60f, 3));
+		return result;
+	}
+
+	private void Update ()
+	{
+		// countdown to 0, then destroy the Timer involved
+		if (countdown != null && countdown.RunTimer())
+		{
+			visibleTimer--;
+		}
+		if (visibleTimer == 0 && startTime == -1)
+		{
+			startTime = Time.time;
+			countdown = null;
+		}
+
+		if (countdown != null)
+			TimerDisplay.TIME_DISPLAY = visibleTimer.ToString();
+		else
+			TimerDisplay.TIME_DISPLAY = DisplayAsStopwatch (Time.time - startTime);
+	}
+}
---
+++ scripts/gameplay/TimerDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TimerDisplay : MonoBehaviour {
+
+	public static string TIME_DISPLAY = string.Empty;
+
+	private void OnGUI ()
+	{
+		float width = Screen.width / 2f;
+		float height = 100f;
+		GUI.Box (new Rect (Screen.width / 2 - width / 2,
+		                   10f,
+		                   width,
+		                   height), TIME_DISPLAY);
+	}
+}
